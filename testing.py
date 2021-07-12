d03942f13183aaba4bfa0961c4ca0fa9619594dc
Keith <61766055+wtvlol@users.noreply.github.com>
2021-07-12 17:44:04 +0800

    Update testing.py

--- testing.py
+++ testing.py
@@ -27,6 +27,7 @@ def thresholdSegment(image,threshold,coords = [],mean = True):
     
     #different grayscale colours
     threshold_cols = [int(255/(len(threshold))+1)*i for i in range(len(threshold)+1)]
+    histr = cv.calcHist([im],[0],None,[256],[0,256])
 
     #mean check
     im_shape = im.shape
@@ -43,7 +44,7 @@ def thresholdSegment(image,threshold,coords = [],mean = True):
     #thresholding
     for i in range(im_r.shape[0]):
         for k in range(len(threshold))[::-1]:
-            if im_r[i] >= threshold[k]:
+            if im_r[i] >= threshold[k] and im_r[i] >= threshold[k-1]:
                 im_r[i] = threshold_cols[k]
                 break
             elif im_r[i] < threshold[k]:
@@ -61,12 +62,29 @@ def thresholdSegment(image,threshold,coords = [],mean = True):
                     im[i][t] = threshold_cols[k-1]
                     break
     """
-    
+    #turning back to original dimensions
     im = im_r.reshape(im_shape[0],im_shape[1])
-    print([set(i) for i in im])
-    plt.imshow(im)
-    ###plot historgram
-#thresholdSegment("testImages/1.jpeg",[0.25,0.5],mean = True)
+    
+    #creating the 2 plots
+    im = im_r.reshape(im_shape[0],im_shape[1])
+    histr_pro = cv.calcHist([im],[0],None,[256],[0,256])
+    
+    fig, axs = plt.subplots(2,2)
+    fig.suptitle("Threshold Segmentation")
+    axs[0,0].imshow(pre)
+    axs[0,0].set_title("Original Image")
+    axs[0,1].imshow(im)
+    axs[0,1].set_title("Processed Image")
+    axs[1,0].plot(histr)
+    axs[1,0].set_title("Original Image Histogram")
+    axs[1,1].plot(histr_pro)
+    axs[1,1].set_title("Processed Image Histogram")
+    plt.tight_layout()
+    
+    #display    
+    plt.show()
+        
+#thresholdSegment("testImages/1.jpeg",[0.5],mean = False)
 
 def edgeContours(image):
     pre = cv.imread(image)
@@ -80,9 +98,6 @@ def edgeContours(image):
     
     #finds the contours
     contours,hierachy = cv.findContours(im,cv.RETR_LIST,cv.CHAIN_APPROX_SIMPLE)
-    print(hierachy)
-    ###object snapshot?
-    ###find highest order parent contour
     
     #convert back to RGB to draw the contours in colour
     im = cv.cvtColor(im,cv.COLOR_GRAY2RGB)
@@ -90,8 +105,18 @@ def edgeContours(image):
     #drawing the contours
     cv.drawContours(im,contours,-1,(0,255,0),2)
     
+    #lebsl the contours
+    for (i,c) in enumerate(contours):
+        M= cv.moments(c)
+        cx= int(M['m10']/M['m00'])
+        cy= int(M['m01']/M['m00'])
+        cv.putText(im, text= str(i), org=(cx,cy),
+                fontFace= cv.FONT_HERSHEY_SIMPLEX, fontScale=0.5, color=(255,0,0),
+                thickness=2, lineType=cv.LINE_AA)
     #display
+    
     plt.imshow(im)
+    plt.title("Identified Contours")
     
     """
     cv.imshow('contours', im)
@@ -99,7 +124,35 @@ def edgeContours(image):
         cv.destroyAllWindows()
     """
 
-edgeContours("testImages/3.png")
+#edgeContours("testImages/3.png")
 
 
 #colour image segmentation
+
+def contourExtract(image,cont_no):
+    pre = cv.imread(image)
+    im = cv.imread(image)
+    
+    #converts image to grayscale
+    im = cv.cvtColor(im,cv.COLOR_BGR2GRAY)
+    
+    #uses average value as global threshold
+    _,im = cv.threshold(im,im.mean(),255,cv.THRESH_BINARY)
+    
+    #finds the contours
+    contours,hierachy = cv.findContours(im,cv.RETR_LIST,cv.CHAIN_APPROX_SIMPLE)
+
+    
+    #convert back to RGB to draw the contours in colour
+    im = cv.cvtColor(im,cv.COLOR_GRAY2RGB)
+    
+    #drawing the contours
+    cv.drawContours(im,contours,-1,(0,255,0),2)
+    
+    x, y, w, h = cv.boundingRect(contours[cont_no])
+    im = im[y:y + h, x:x + w]
+    #display
+    plt.imshow(im)
+    plt.title("Contour Number: {}".format(cont_no))
+
+contourExtract("testImages/3.png",4)
